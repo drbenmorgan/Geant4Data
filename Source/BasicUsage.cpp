f0d95d4cdddc744b1b2811a043d50bd2378bbe07
Ben Morgan <bmorgan.warwick@gmail.com>
2012-05-17 19:07:18 +0100

    Included Boost license and applied it to files.

--- Source/BasicUsage.cpp
+++ Source/BasicUsage.cpp
@@ -2,6 +2,9 @@
 //
 // Copyright (c) 2012 by Ben Morgan <bmorgan.warwick@gmail.com> 
 // Copyright (c) 2012 by The University of Warwick
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
 
@@ -9,6 +12,4 @@ int main() {
   std::cout << "[BasicUsage::" << __LINE__ << "]" << std::endl;
   return 0;
 }
-
-
 
